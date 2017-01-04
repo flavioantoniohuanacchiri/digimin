3b40a1e245ae08b483bc0416439db66266ddafb1
Flavio Huanacchiri <fhuanacchiri@hdec.com.pe>
2017-01-03 21:38:36 -0500

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,25 +4,24 @@
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
             <br /><br /><br />
-               @section ('login_panel_title','Please Sign In')
+               @section ('login_panel_title','Logueate')
                @section ('login_panel_body')
                         <form method="post" action="{{ url('/login') }}">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                             <fieldset>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="E-mail" name="username" type="text" autofocus>
+                                    <input class="form-control" placeholder="Usuario" name="username" type="text" autofocus>
                                 </div>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                                    <input class="form-control" placeholder="Contraseña" name="password" type="password" value="">
                                 </div>
                                 <div class="checkbox">
                                     <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                                        <input name="remember" type="checkbox" value="Remember Me">Recuerdame
                                     </label>
                                 </div>
                                 <!-- Change this to a button or input when using this as a form -->
                                 <input type="submit" name="" class="btn btn-lg btn-success btn-block" value="Login">
-                                <a href="{{ url ('') }}" class="btn btn-lg btn-success btn-block">Login</a>
                             </fieldset>
                         </form>
                     
